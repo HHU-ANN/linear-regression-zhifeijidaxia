36d9edfb92e121ddc1ad65e6c8ffc9fd3a445f46
zhifeijidaxia <1146650581@qq.com>
2023-05-02 00:47:38 +0800

    第二次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -8,7 +8,6 @@ except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
 
-
 def ridge(data):
     # 先求出(A^T*A+cI)xhat
     X, Y = read_data()
@@ -22,18 +21,30 @@ def ridge(data):
     temp2 = np.linalg.pinv(temp)
     w = np.matmul(np.matmul(temp2, X), Y)
 
-    # 将data函数增添一列1扩为增广矩阵
-    add2 = np.ones(len(data)).reshape(len(data), 1)
-    data = np.c_[data, add2]
-    # result数组
-    result = np.zeros(len(data))
+    result = 0
     for i in range(len(data)):
-        result[i] = np.dot(w, data[i])
+        result+=w[i]*data[i]
+    result+= w[len(data)]
     return result
 
-
 def lasso(data):
-    return data
+    # 先求出(A^T*A+cI)xhat
+    X, Y = read_data()
+    X = X.transpose()
+    add = np.ones(len(Y)).reshape(1, len(Y))
+    # 将X置为增广矩阵
+    X = np.r_[X, add]
+    const = 0.6
+    # 以下为通过最小二乘法得出权重向量w
+    temp = np.matmul(X, X.transpose()) + const * np.identity(7)
+    temp2 = np.linalg.pinv(temp)
+    w = np.matmul(np.matmul(temp2, X), Y)
+
+    result = 0
+    for i in range(len(data)):
+        result += w[i] * data[i]
+    result += w[len(data)]
+    return result
 
 
 # 原路径为def read_data(path='./data/exp02/'):
